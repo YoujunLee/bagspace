2944068ac5722699f7134e3a44fe2613c3a5e96c
YoujunLee <injxyj@gmail.com>
2017-01-16 19:54:39 +0900

    menu list print 성공

--- bagspace_application/src/pages/tabs/tabs.ts
+++ bagspace_application/src/pages/tabs/tabs.ts
@@ -4,28 +4,8 @@ import { MenuController } from 'ionic-angular';
 
 @Component({
   template: `
-       <ion-menu side="left" persist='true' [content]="mycontent">
-          <ion-header>
-            <ion-toolbar>
-              <ion-title>Pages</ion-title>
-            </ion-toolbar>
-          </ion-header>
-          <ion-content>
-            <ion-list>
-              <button ion-item>
-                Login
-              </button>
-              <button ion-item>
-                Signup
-              </button>
-            </ion-list>
-          </ion-content>
-      </ion-menu>
-
-
-    
     <ion-header>
-      <ion-navbar #mycontent [color]="isAndroid ? 'danger' : 'primary'">
+     <ion-navbar #mycontent [color]="isAndroid ? 'danger' : 'primary'">
           <ion-buttons start>
             <button ion-button menuToggle icon-only>
               <ion-icon name="menu"></ion-icon>
@@ -34,16 +14,26 @@ import { MenuController } from 'ionic-angular';
           <ion-title>bagspace</ion-title>
       </ion-navbar>
     </ion-header>
+   
+
     <ion-content>
     </ion-content>
+     <ion-menu side="left" persistent="true" [content]="mycontent">
+      <ion-toolbar>
+         <ion-title>Menu</ion-title>
+      </ion-toolbar>
+      <ion-list>
+         <button ion-item>Login</button>
+         <button ion-item>Signup</button>
+      </ion-list>
+    </ion-menu>
 `})
 export class TabsPage {
    constructor(public menuCtrl: MenuController) {}
- openMenu() {
+   openMenu() {
    this.menuCtrl.open();
- }
- 
-}``
+   }
+}
 
 @Component({
   templateUrl: 'tabs.html'
