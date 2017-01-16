a737a47e787a94a99c9dfda96d45b88aabb946c1
CheolWoong Choi <bluesky4381@naver.com>
2017-01-16 22:20:34 +0900

    출발지, 도착지 : actionSheet 적용, 가는 날짜 : dateTime 적용 (수정 필요)

--- bagspace_application/src/pages/mover-first/mover-first.ts
+++ bagspace_application/src/pages/mover-first/mover-first.ts
@@ -1,22 +1,66 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
-/*
-  Generated class for the MoverFirst page.
+import { Platform, ActionSheetController } from 'ionic-angular';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-mover-first',
   templateUrl: 'mover-first.html'
 })
 export class MoverFirstPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http,
+  public platform: Platform, public actionsheetCtrl: ActionSheetController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MoverFirstPage');
+  }
+
+placeSelect() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: '장소 선택',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: '서울',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: '대전',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: '대구',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: '부산',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: '포항',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
