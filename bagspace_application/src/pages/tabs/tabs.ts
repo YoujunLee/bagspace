64db2ef3d43c0eafa2706a21edd45ef218b75b69
YoujunLee <injxyj@gmail.com>
2017-01-16 21:23:27 +0900

    하단부 아이콘 변경

--- bagspace_application/src/pages/tabs/tabs.ts
+++ bagspace_application/src/pages/tabs/tabs.ts
@@ -1,38 +1,17 @@
 import { Component } from '@angular/core';
-import { MenuController } from 'ionic-angular';
-
 
 @Component({
   template: `
     <ion-header>
-     <ion-navbar #mycontent [color]="isAndroid ? 'danger' : 'primary'">
-          <ion-buttons start>
-            <button ion-button menuToggle icon-only>
-              <ion-icon name="menu"></ion-icon>
-            </button>
-          </ion-buttons>
-          <ion-title>bagspace</ion-title>
+     <ion-navbar [color]="isAndroid ? 'danger' : 'primary'">
+          <ion-searchbar placeholder="어디로 시나요?" (click)="getItems($event)"></ion-searchbar>
       </ion-navbar>
     </ion-header>
-   
-
     <ion-content>
     </ion-content>
-     <ion-menu side="left" persistent="true" [content]="mycontent">
-      <ion-toolbar>
-         <ion-title>Menu</ion-title>
-      </ion-toolbar>
-      <ion-list>
-         <button ion-item>Login</button>
-         <button ion-item>Signup</button>
-      </ion-list>
-    </ion-menu>
 `})
 export class TabsPage {
-   constructor(public menuCtrl: MenuController) {}
-   openMenu() {
-   this.menuCtrl.open();
-   }
+  
 }
 
 @Component({
