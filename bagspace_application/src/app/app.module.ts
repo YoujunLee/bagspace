58d507f2fdf857eb87deda4739185ed7e1eda066
YoujunLee <injxyj@gmail.com>
2017-01-16 22:47:05 +0900

    충돌 문제 해결

--- bagspace_application/src/app/app.module.ts
+++ bagspace_application/src/app/app.module.ts
@@ -2,24 +2,16 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-<<<<<<< HEAD
 import { TabsPage, IconPage } from '../pages/tabs/tabs';
 import { UpperTabsPage } from '../pages/upper-tabs/upper-tabs';
-=======
-import { MoverFirstPage } from '../pages/mover-first/mover-first';
->>>>>>> a737a47e787a94a99c9dfda96d45b88aabb946c1
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-<<<<<<< HEAD
     TabsPage,
     IconPage,
     UpperTabsPage
-=======
-    MoverFirstPage
->>>>>>> a737a47e787a94a99c9dfda96d45b88aabb946c1
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -28,13 +20,9 @@ import { MoverFirstPage } from '../pages/mover-first/mover-first';
   entryComponents: [
     MyApp,
     HomePage,
-<<<<<<< HEAD
     TabsPage,
     IconPage,
     UpperTabsPage
-=======
-    MoverFirstPage
->>>>>>> a737a47e787a94a99c9dfda96d45b88aabb946c1
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- bagspace_application/src/pages/home/home.ts
+++ bagspace_application/src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-import { MoverFirstPage } from '../mover-first/mover-first';
 
 @Component({
   selector: 'page-home',
@@ -14,12 +13,5 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
   
   }
-<<<<<<< HEAD
-=======
 
-  goMoverFirst(){
-    this.navCtrl.push(MoverFirstPage);
-  }
-
->>>>>>> a737a47e787a94a99c9dfda96d45b88aabb946c1
 }
