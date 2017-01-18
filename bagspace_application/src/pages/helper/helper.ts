eaa2f74916fa146fa37b139ed55c93c551579bb8
YoujunLee <injxyj@gmail.com>
2017-01-18 11:25:11 +0900

    의미 없는 commit

--- bagspace_application/src/pages/helper/helper.ts
+++ bagspace_application/src/pages/helper/helper.ts
@@ -1,18 +1,12 @@
 import { Component} from '@angular/core';
-import { Platform, NavParams, ViewController  } from 'ionic-angular';
+import {Platform, NavParams, ViewController} from 'ionic-angular';
 
-/*
-  Generated class for the Helper page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-helper',
   templateUrl: 'helper.html'
 })
 export class HelperPage {
-
   constructor(public platform: Platform, public params: NavParams, public viewCtrl: ViewController) {}
   dismiss() {
     this.viewCtrl.dismiss();
