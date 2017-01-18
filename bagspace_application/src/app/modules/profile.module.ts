cfc1963181b2eed1fa42adfe76308d6eb39709a9
YoujunLee <injxyj@gmail.com>
2017-01-18 15:38:38 +0900

    setting group화

--- bagspace_application/src/app/modules/profile.module.ts
+++ bagspace_application/src/app/modules/profile.module.ts
@@ -3,16 +3,13 @@ import {IonicModule, IonicErrorHandler } from 'ionic-angular';
 import {CommonModule} from '@angular/common';
 import { ProfilePage } from '../../pages/profile_group/profile/profile';
 import { HelperPage } from '../../pages/profile_group/helper/helper';
+import { SettingPage } from '../../pages/profile_group/setting/setting';
 
 
 @NgModule({
-  declarations: [
-    ProfilePage, HelperPage
-  ],
+  declarations: [ProfilePage, HelperPage, SettingPage],
   imports: [IonicModule, CommonModule],
-  entryComponents: [
-    ProfilePage, HelperPage
-    ],
+  entryComponents: [ProfilePage, HelperPage, SettingPage],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class ProfileModule {}
